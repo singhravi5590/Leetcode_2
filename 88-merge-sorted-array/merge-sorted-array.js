c2e687831e516cb71d3e43c8348775a3ccb63cbd
singhravi5590 <singhravi5590@gmail.com>
2025-08-30 12:01:22 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (66.21%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,19 +6,9 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-   let i = m - 1; 
-   let j = n - 1;
-   let k = m + n - 1;
+   let mergeArray = nums1.slice(0, m).concat(nums2).sort((a,b) => a-b);
 
-   while(j >= 0){
-    if(i >= 0 && nums1[i] > nums2[j]){
-        nums1[k] = nums1[i];
-        i--;
-    }
-    else{
-        nums1[k] = nums2[j];
-        j--
-    }
-    k--;
+   for(let i = 0; i<mergeArray.length; i++){
+    nums1[i] = mergeArray[i];
    }
 };

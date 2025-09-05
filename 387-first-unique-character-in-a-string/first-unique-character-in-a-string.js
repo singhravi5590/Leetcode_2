a12b07eeaa4f9803d2a63a66e97d8f40e445f86f
singhravi5590 <singhravi5590@gmail.com>
2025-09-06 00:18:50 +0530

    Time: 35 ms (45.96%) | Memory: 59.1 MB (72.86%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -3,18 +3,17 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let freq = {};
-
-    // Count frequencies
-    for (let ch of s) {
-        freq[ch] = (freq[ch] || 0) + 1;
+    let charMap = {};
+    
+    for(let i = 0; i<s.length; i++){
+        charMap[s[i]] = charMap[s[i]] ? charMap[s[i]] + 1 : 1
     }
-
-    // Find first unique character
-    for (let i = 0; i < s.length; i++) {
-        if (freq[s[i]] === 1) {
+    
+    for(let i = 0; i<s.length; i++){
+        if(charMap[s[i]] === 1){
             return i;
         }
     }
+    
     return -1;
 };

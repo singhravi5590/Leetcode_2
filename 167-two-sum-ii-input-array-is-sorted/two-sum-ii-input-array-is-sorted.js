e89ef227ec9bc42b248fe7f1e12d366c53701431
singhravi5590 <singhravi5590@gmail.com>
2025-09-19 23:07:13 +0530

    Time: 2 ms (63.02%) | Memory: 57 MB (13.48%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -4,11 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    for(let i = 0; i<numbers.length; i++){
-        for(let j = i+1; j<numbers.length; j++){
-            if(numbers[i] + numbers[j] === target){
-            return [i+1, j+1]
-            }
+    let i = 0;
+    let j = numbers.length - 1;
+    while(i < j){
+        let sum = numbers[i] + numbers[j];
+        if(sum === target){
+            return [i+1, j+1];
+        }
+        else if(sum > target){
+            j--
+        }
+        else{
+            i++
         }
     }
 };

535c1ac7929782a3ae95d076666e0b58a2e0446c
singhravi5590 <singhravi5590@gmail.com>
2025-10-05 21:52:40 +0530

    Time: 4 ms (51.00%) | Memory: 62.4 MB (10.95%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.js
+++ 459-repeated-substring-pattern/repeated-substring-pattern.js
@@ -4,10 +4,10 @@
  */
 var repeatedSubstringPattern = function(s) {
     let n = s.length;
-    for (let i = 1; i <= n / 2; i++) {
-        if (n % i === 0) {
+    for(let i = 1; i<=n/2; i++){
+        if(n % i === 0){
             let sub = s.slice(0, i);
-            if (sub.repeat(n / i) === s) return true;
+            if(sub.repeat(n/i) === s) return true;
         }
     }
     return false;
